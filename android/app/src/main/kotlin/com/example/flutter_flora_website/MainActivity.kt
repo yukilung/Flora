e0deb4a1e8cfd484c28de2bccc3223af59aaeb60
yukilung <54215473+yukilung@users.noreply.github.com>
2023-12-13 12:41:44 +0800

    magre web develop

---
+++ android/app/src/main/kotlin/com/example/flutter_flora_website/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_flora_website
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/web.dart
+++ lib/web.dart
@@ -1,34 +1,29 @@
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_flora_website/web.dart';
 
-void main() {
-  if (!kIsWeb) {
-    runApp(MyApp());
-  } else {
-    runApp(MyWeb());
-  }
-}
-
-class MyApp extends StatelessWidget {
+class MyWeb extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('This is title'),
+        ),
+        body: Center(
+          child: Text('This is HomePage'),
+        ));
   }
 }
